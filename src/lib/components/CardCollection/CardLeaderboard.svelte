2271da118ff6fa3f35ee91d178c68cc92c7b87c8
Camilo Di Paolo <camilodipaolo10@gmail.com>
2023-05-06 19:41:51 -0300

    fix leaderboard

--- src/lib/components/CardCollection/CardLeaderboard.svelte
+++ src/lib/components/CardCollection/CardLeaderboard.svelte
@@ -5,7 +5,7 @@
 
 <div class="relative">
 	<button
-		class="bg-gradient-to-br z-10 border border-stone-800 p-2 sm:rounded-xl rounded-md flex justify-center items-center relative backdrop-blur-sm shadow-lg duration-75 sm:text-[25px] text-[12px] sm:w-20 sm:h-24 h-10"
+		class="bg-gradient-to-br z-10 border border-stone-800 p-2 sm:rounded-xl rounded-md flex justify-center items-center relative backdrop-blur-sm shadow-lg duration-75 sm:text-[25px] text-[12px] sm:w-20 sm:h-24 h-10 w-6"
 		class:shadow-tier-1={tier === 1}
 		class:shadow-tier-2={tier === 2}
 		class:shadow-tier-3={tier === 3}
--- src/routes/(app)/leaderboard/+page.svelte
+++ src/routes/(app)/leaderboard/+page.svelte
@@ -10,20 +10,23 @@
 
 <AmazingTitle>Leaderboard</AmazingTitle>
 <div
-	class="flex flex-col border border-fuchsia-400 sm:p-10 p-2 rounded-lg bg-fuchsia-200/10 backdrop-blur-lg h-2/3 overflow-y-scroll"
+	class="flex flex-col gap-2 border border-fuchsia-400 sm:p-10 p-2 rounded-lg bg-fuchsia-200/10 backdrop-blur-lg h-2/3 overflow-y-scroll"
 >
 	{#each leaderboard.sort((user1, user2) => {
 		const score1 = user1.tier_3 * 10 + user1.tier_2 * 5 + user1.tier1;
 		const score2 = user2.tier_3 * 10 + user2.tier_2 * 5 + user2.tier1;
 		return score2 - score1;
 	}) as score}
-		<div class="flex justify-start items-center sm:gap-10 gap-2">
-			<img src={score.picture} class="rounded-full sm:w-16 sm:h-16 w-8 h-8" />
-			<p class="sm:text-md text-sm">{score.username}</p>
-
-			<CardLeaderboard tier={3} number={score.tier_3} />
-			<CardLeaderboard tier={2} number={score.tier_2} />
-			<CardLeaderboard tier={1} number={score.tier_1} />
+		<div class="flex justify-between items-center sm:gap-10 gap-2">
+			<div class="flex gap-2 justify-start items-center">
+				<img src={score.picture} class="rounded-full sm:w-16 sm:h-16 w-8 h-8" />
+				<p class="sm:text-md text-sm">{score.username}</p>
+			</div>
+			<div class="flex gap-2">
+				<CardLeaderboard tier={3} number={score.tier_3} />
+				<CardLeaderboard tier={2} number={score.tier_2} />
+				<CardLeaderboard tier={1} number={score.tier_1} />
+			</div>
 		</div>
 	{/each}
 </div>
